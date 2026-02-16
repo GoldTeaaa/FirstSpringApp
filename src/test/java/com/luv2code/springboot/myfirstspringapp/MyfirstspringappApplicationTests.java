fb02ed0cc97feb77b8daff6af381c0a2090f104f
Handay <handayariawan@gmail.com>
2026-02-16 20:06:57 +0700

    first commit

---
+++ src/test/java/com/luv2code/springboot/myfirstspringapp/MyfirstspringappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.myfirstspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirstspringappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
