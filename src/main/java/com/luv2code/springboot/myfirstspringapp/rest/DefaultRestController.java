4094751b105fccadc6bff871922cf6d5c7f1783b
GoldTeaaa <handayariawan@gmail.com>
2026-02-16 22:44:58 +0700

    Change java SDK version dependency

---
+++ src/main/java/com/luv2code/springboot/myfirstspringapp/rest/DefaultRestController.java
@@ -0,0 +1,19 @@
+package com.luv2code.springboot.myfirstspringapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DefaultRestController {
+
+    @GetMapping("/")
+    public String HelloWorld(){
+        return "Hello World!";
+    }
+
+    @GetMapping("/hi")
+    public String HiWorld(){
+        return "Hi World!";
+    }
+
+}
